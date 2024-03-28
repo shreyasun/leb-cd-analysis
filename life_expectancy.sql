b0c227fad563a9122e2ce2c27cf7d88a544408cd
Shreya Sundar <shreyasundar27@gmail.com>
2024-03-28 14:16:18 -0700

    added more queries

--- life_expectancy.sql
+++ life_expectancy.sql
@@ -1,109 +1,136 @@
-CREATE DATABASE life_expectancy_disease_prediction;
+CREATE DATABASE IF NOT EXISTS life_expectancy_disease_prediction;
+USE life_expectancy_disease_prediction;
 
-SELECT * FROM life_expectancy_disease_prediction.leb;
-DELETE FROM life_expectancy_disease_prediction.leb WHERE `Country Name`='Country Name';
-SELECT * FROM life_expectancy_disease_prediction.ncd;
+CREATE TABLE leb (
+	`Country Name` TEXT, 
+    `2000` DOUBLE,
+    `2010` DOUBLE,
+    `2015` DOUBLE,
+    `2019` DOUBLE
+);
+
+/*
+LOAD DATA INFILE 'life_expectancy.csv'
+INTO TABLE leb
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+*/
+
+SELECT * FROM leb;
+DELETE FROM leb WHERE `Country Name`='Country Name';
 
 /** LOWEST LIFE EXPECTANCIES **/
-/* What country had the lowest average life expectancy for 2000? */
+/* Country with the lowest average life expectcomancy for each year (2000, 2010, 2015, 2019) */
 SELECT `Country Name`, MIN(`2000`) as lowest_leb_2000
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 GROUP BY `Country Name`
 ORDER BY lowest_leb_2000 asc
 LIMIT 1;
 
-/* What country had the lowest average life expectancy for 2010? */
 SELECT `Country Name`, MIN(`2010`) as lowest_leb_2010
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 GROUP BY `Country Name`
 ORDER BY lowest_leb_2010 asc
-LIMIT 10;
+LIMIT 1;
 
-/* What country had the lowest average life expectancy for 2015? */
 SELECT `Country Name`, MIN(`2015`) as lowest_leb_2015
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 GROUP BY `Country Name`
 ORDER BY lowest_leb_2015 asc
-LIMIT 10;
+LIMIT 1;
 
-/* What country had the lowest average life expectancy in 2019? */
 SELECT `Country Name`, MIN(`2019`) as lowest_leb_2019
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 GROUP BY `Country Name`
 ORDER BY lowest_leb_2019 asc
-LIMIT 10;
+LIMIT 1;
 
 /* alternate way to write with subquery */
 SELECT `Country Name`, `2019` as lowest_leb_2019
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 WHERE `2019` = (SELECT MIN(`2019`) FROM life_expectancy_disease_prediction.leb);
 
 /* Percent Difference in lowest life expectancy from 2000 - 2019 */
 SELECT MIN(`2019`) as lowest_leb_2019, MIN(`2000`) as lowest_leb_2000, ((MIN(`2019`) - MIN(`2000`)) / MIN(`2000`)) * 100 as percent_diff
-FROM life_expectancy_disease_prediction.leb;
+FROM leb;
 
 /********************************************************************************************************************/
 /** HIGHEST LIFE EXPECTANCIES **/
 
-DELETE FROM life_expectancy_disease_prediction.leb WHERE `Country Name`='Country Name';
-
-/* What country had the highest average life expectancy for 2000? */
+/* Country with the highest average life expectancy for each year (2000, 2010, 2015, 2019) */
 SELECT `Country Name`, MAX(`2000`) as highest_leb_2000
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 GROUP BY `Country Name`
 ORDER BY highest_leb_2000 desc
-LIMIT 10;
+LIMIT 1;
 
-/* What country had the highest average life expectancy for 2010? */
 SELECT `Country Name`, MAX(`2010`) as highest_leb_2010
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 GROUP BY `Country Name`
 ORDER BY highest_leb_2010 desc
-LIMIT 10;
+LIMIT 1;
 
-/* What country had the highest average life expectancy for 2015? */
 SELECT `Country Name`, MAX(`2015`) as highest_leb_2015
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 GROUP BY `Country Name`
 ORDER BY highest_leb_2015 desc
-LIMIT 10;
+LIMIT 1;
 
-/* What country had the highest average life expectancy in 2019? */
 SELECT `Country Name`, MAX(`2019`) as highest_leb_2019
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 GROUP BY `Country Name`
 ORDER BY highest_leb_2019 desc
-LIMIT 10;
+LIMIT 1;
 
-/* Percent Difference in highest life expectancy from 2000 - 2019 */
+/* Percent Change in highest life expectancy from 2000 - 2019*/
 SELECT MAX(`2019`) as highest_leb_2019, MAX(`2000`) as highest_leb_2000, ((MAX(`2019`) - MAX(`2000`)) / MAX(`2000`)) * 100 as percent_diff
-FROM life_expectancy_disease_prediction.leb;
+FROM leb;
 
 /********************************************************************************************************************/
-/** AVERGAGE LIFE EXPECTANCIES **/
+/** AVERAGE LIFE EXPECTANCIES **/
 
-/* World Average Life Expectancy in 2000, 2010, 2015, 2019 */
+/* Average life expectancy globally for each year (2000, 2010, 2015, 2019). */
 SELECT AVG(`2000`) as avg_leb_2000, AVG(`2010`) as avg_leb_2010, AVG(`2015`) as avg_leb_2015, AVG(`2019`) as avg_leb_2019
-FROM life_expectancy_disease_prediction.leb;
+FROM leb;
 
-/* Change in average life expectancy from 2000 to 2019 */
+/* Percent Change in average life expectancy from 2000 - 2019 */
 SELECT AVG(`2000`) as avg_leb_2000, AVG(`2019`) as avg_leb_2019, ((AVG(`2019`) - AVG(`2000`)) / AVG(`2000`)) * 100 as percent_diff
-FROM life_expectancy_disease_prediction.leb;
+FROM leb;
 
-/* Country with largest change in average life expectancy from 2000 to 2019 */
+/* Country has the largest change in life expectancy from 2000 to 2019 */
 SELECT `Country Name`, AVG(`2000`) as avg_leb_2000, AVG(`2019`) as avg_leb_2019, ((AVG(`2019`) - AVG(`2000`)) / AVG(`2000`)) * 100 as percent_diff
-FROM life_expectancy_disease_prediction.leb
+FROM leb
 GROUP BY `Country Name`
-ORDER BY percent_diff desc;
+ORDER BY percent_diff desc
+LIMIT 1;
 
 /* Countries where life expectancy decreased from 2000 to 2019 */
-SELECT `Country Name`, `2000` as avg_leb_2000, `2019` as avg_leb_2019
-FROM life_expectancy_disease_prediction.leb
+SELECT `Country Name`, `2000` as avg_leb_2000, `2019` as avg_leb_2019, (((`2019` - `2000`) / `2000`) * 100) as percent_diff
+FROM leb
 WHERE `2019` < `2000`;
 
-/* Countries where life expectancy is above global average in 2019 */
-SELECT `Country Name`, `2019` as leb_2019
-FROM life_expectancy_disease_prediction.leb
-WHERE `2019` > (SELECT AVG(`2019`) FROM life_expectancy_disease_prediction.leb)
+/*Total number of countries*/
+SELECT COUNT(`2000`)
+FROM leb;
+
+/* Number of countries where life expectancy is above global average per year (2000, 2010, 2015, 2019) */
+
+SELECT COUNT(*)
+FROM leb
+WHERE `2000` > (SELECT AVG(`2000`) FROM leb);
+
+SELECT COUNT(*)
+FROM leb
+WHERE `2010` > (SELECT AVG(`2010`) FROM leb);
+
+SELECT COUNT(*)
+FROM leb
+WHERE `2015` > (SELECT AVG(`2015`) FROM leb);
+
+SELECT COUNT(*)
+FROM leb
+WHERE `2019` > (SELECT AVG(`2019`) FROM leb);
+
 
 
