dd5ca16cf395936bd2d5c3915a430941ad457eed
Shreya Sundar <shreyasundar27@gmail.com>
2024-03-28 14:33:16 -0700

    Planning joins

---
+++ join.sql
@@ -0,0 +1,9 @@
+
+
+/* In which countries did the life expectancy increase and mortality rate decrease from 2000 to 2019? */
+
+/* In which countries did the life expectancy decrease and mortality rate increase from 2000 to 2019? */
+
+/* What is the average life expectancy of countries in 2019 where the mortality rate increased by more than 5% between 2000 and 2019?*/
+
+/* What is the average life expectancy of countries in 2019 where the mortality rate decreased by more than 20% between 2000 and 2019? */
